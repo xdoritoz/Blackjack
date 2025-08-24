5816d8ed1a29d4b062d7681b55d0db0e6f64f1cb
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2025-08-24 15:45:14 -0500

    aaa

--- src/main/java/org/example/Crupier.java
+++ src/main/java/org/example/Crupier.java
@@ -12,31 +12,17 @@ public class Crupier extends Jugador {
     public void jugar(Baraja baraja){
         List<Integer> pos = new ArrayList<>();
 
-        setPuntuacion(calcularPuntos());
+        calcularPuntos();
 
         while (getPuntuacion() < 17) {
 
             Carta nueva = baraja.removerCarta(baraja.getBaraja().removeFirst());
             pedirCarta(nueva);
 
-            setPuntuacion(getPuntuacion() + nueva.getPuntos());
-            setPuntuacion(getPuntuacion());
-
-            //modificarAses();
-
-            pos = posicionA(getMano());
-
-            if (getPuntuacion() > 21 && pos != null) {
-                for (int i = 0; i < pos.size(); i++) {
-                    if (getPuntuacion() > 21) {
-                        mano.get(pos.get(i)).setPuntos(1);
-                        setPuntuacion(getPuntuacion() - 10);
-                    }
-                }
-            }
+            calcularPuntos();
 
         }
-        System.out.println("Aces: " + posicionA(getMano()));
+        System.out.println("Aces: " + posicionA());
         System.out.println(getMano());
         System.out.println("el crupier se planta con: " + getPuntuacion() + " puntos");
     }
