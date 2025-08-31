1de60c4ece54fa80040c1ea4e7b484e812f655a5
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2025-08-31 01:36:33 -0500

    delegation of responsabilities in functions

--- src/main/java/org/example/Juego.java
+++ src/main/java/org/example/Juego.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.exception.InsufficientBalanceException;
+
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -7,16 +9,12 @@ import java.util.Scanner;
 
 public class Juego {
     public void iniciarJuego() {
-
-        List<Jugador> jugadores = new ArrayList<Jugador>();
-
-        Jugador jugador = new Jugador("fede", 10000, "jugador");
         Crupier crupier = new Crupier();
         Baraja baraja = new Baraja();
 
         baraja.llenarBaraja();
 
-        repartirMano(jugador, crupier);
+        //repartirMano(jugador, crupier);
 
         int opcion = 0, opcion2;
         boolean salir = false;
@@ -28,7 +26,7 @@ public class Juego {
             errorOcurrido = false;
 
             try {
-                System.out.println("Escribe 1 si quieres iniciar el juego, 0 si quieres salir. \n");
+                System.out.println("Escribe 1 si quieres iniciar el juego, 0 si quieres salir : \n");
                 opcion = sc.nextInt();
 
                 if (opcion != 0 && opcion != 1) {
@@ -47,25 +45,73 @@ public class Juego {
             }
         } while (errorOcurrido);
 
-            if (opcion == 1) {
-                do {
-                    System.out.println("## EL JUEGO A COMENZADO ##");
-                    System.out.println("Tu numero de creditos es: " + jugador.getCreditos());
+        if (opcion == 1) {
+            sc.nextLine();
+
+            int cantJugadores = 0;
+            String nombre = "";
+
+            do {
+                System.out.println("## EL JUEGO A COMENZADO ##");
+
+                System.out.print("Ingrese la cantidad de jugadores: ");
+                cantJugadores = Integer.parseInt(sc.nextLine());
+
+                //Creacion de jugadores
+                for (int i = 0; i < cantJugadores; i++) {
+                    System.out.print("\nDigite el nombre del jugador " + "[" + i + "]: ");
+                    nombre = sc.nextLine();
 
 
+                    Jugador jugador = new Jugador(nombre, 10000, "jugador");
+                    jugadores.add(jugador);
 
+                    //Apuestas de los jugadores
+                    pedirAPuesta(jugadores.get(i));
 
+                }
 
-                    System.out.println("¿quieres Continuar con el juego? 1 para SI 0 para NO");
-                    opcion2 = sc.nextInt();
-                    salir = (opcion2 == 0);
+                System.out.println(jugadores.toString());
 
-                } while (!salir);
+                System.out.println("¿Quieres Continuar con el juego? 1 para SI 0 para NO:");
+                opcion2 = sc.nextInt();
+                salir = (opcion2 == 0);
+
+            } while (!salir);
+        }
+
+    }
+
+    public void repartirMano (Jugador jugador, Crupier crupier){
+
+    }
+
+    public void pedirAPuesta (Jugador jugador) {
+        boolean apuestaValida = false;
+        Scanner sc = new Scanner(System.in);
+
+        while (!apuestaValida) {
+            try {
+                System.out.print("COLOCA TU APUESTA💵💵💵💵: ");
+                apuesta = sc.nextInt();
+                sc.nextLine();
+
+                jugador.colocarApuesta(apuesta);
+                apuestaValida = true;
+
+            } catch (InsufficientBalanceException e) {
+                System.out.println("\n" + e.getMessage());
+
+            } catch (InputMismatchException e) {
+                System.out.println("\n" + jugador.getNombre() + " TIENE QUE SER NUMERO ESTUPIDOOOOOOOOOOOO");
+                sc.nextLine();
             }
 
-    }
-
-    public void repartirMano(Jugador jugador, Crupier crupier) {
+        }
 
     }
+
 }
+
+
+
